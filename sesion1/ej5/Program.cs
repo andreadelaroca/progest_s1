9abbec57122291e536029e32afb1b21b51dba0b7
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-08-31 16:54:30 -0600

    Ejercicio 5 - S1

--- sesion1/ej5/Program.cs
+++ sesion1/ej5/Program.cs
@@ -2,3 +2,14 @@
 en un día en una lechería de Estelí, utilizando como entradas el volumen promedio 
 por vaca en litros y el número total de vacas ordeñadas, ambos proporcionados por 
 el usuario (1 balde = 20 litros). */
+
+Console.WriteLine("Calculo de total de baldes de leche producida en un dia");
+
+Console.Write("Ingrese el volumen promedio en litros: ");
+double v = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ingrese el numero total de vacas ordeñadas: ");
+int vacas = Convert.ToInt32(Console.ReadLine());
+
+double baldes = (v * vacas) / 20;
+Console.WriteLine($"La cantidad total de baldes producido es de {baldes}l");
