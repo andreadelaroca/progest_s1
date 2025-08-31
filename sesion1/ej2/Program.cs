03473f6373fd8aba3c29e7bf90f55421d4ed3edb
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-08-31 16:36:25 -0600

    Ejercicio 2 - S1

--- sesion1/ej2/Program.cs
+++ sesion1/ej2/Program.cs
@@ -1,4 +1,12 @@
-﻿/* DDesarrolle un programa que calcule el tiempo total en horas de un viaje en bus 
+﻿/* Desarrolle un programa que calcule el tiempo total en horas de un viaje en bus 
 desde Managua a Granada, donde el usuario ingresa la velocidad promedio en kilómetros 
 por hora como entrada, asumiendo una distancia fija de 40 kilómetros. */
 
+Console.Write("Ingrese la velocidad promedio en Km/h: ");
+double velocidad = Convert.ToDouble(Console.ReadLine());
+
+int distancia = 40;
+Console.WriteLine($"La distancia fija es de {distancia}Km");
+
+double tiempo = distancia / velocidad;
+Console.WriteLine($"El tiempo total de viaje es de {tiempo} horas.");
