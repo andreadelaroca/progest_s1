4856bca73c3e90d618bbd3f4496095798eaf3b46
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-08-31 16:29:47 -0600

    Ejercicio 1 S1

--- sesion1/ej1/Program.cs
+++ sesion1/ej1/Program.cs
@@ -2,3 +2,14 @@
 donde el usuario ingresa la longitud y el ancho en varas (1 manzana = 1,428.8 varas²), 
 utilizando una cantidad fija de 5 varas de ancho y la longitud como entrada. */
 
+Console.Write("Ingrese la longitud del terreno en varas: ");
+double longitud = Convert.ToDouble(Console.ReadLine());
+
+int ancho = 5;
+Console.Write($"El valor fijo del ancho son {ancho} varas.");
+
+double areaVaras = longitud * ancho;
+double areaManzanas = areaVaras / 1428.8;
+
+Console.WriteLine($"El area en varas cuadradas es de {areaVaras}");
+Console.WriteLine($"El area en manzanas es de {areaManzanas}");
