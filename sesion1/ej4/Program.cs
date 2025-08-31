41f08b10e5c7b6c2cbae8608e1fcef6da87d7b39
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-08-31 16:49:16 -0600

    Ejercicio 4 - S1

--- sesion1/ej4/Program.cs
+++ sesion1/ej4/Program.cs
@@ -1,3 +1,20 @@
 ﻿/* Desarrolle un programa que calcule el volumen total en metros cúbicos de agua 
 recolectada en un tanque en una finca de Masaya, utilizando como entradas la longitud, 
 el ancho y la altura del tanque, todos proporcionados por el usuario.*/
+
+Console.WriteLine("Calculo del volumen de un tanque de agua");
+
+Console.Write("Ingrese la unidad de medida: ");
+string medida = Console.ReadLine();
+
+Console.Write("Ingrese la longitud del tanque: ");
+double l = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ingrese el ancho del tanque: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ingrese la altura del tanque: ");
+double h = Convert.ToDouble(Console.ReadLine());
+
+double v = l * a * h;
+Console.WriteLine($"El volumen del tanque es de {v}{medida}");
