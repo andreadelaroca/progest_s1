96e88f4fa79ab0af211abd8db2002cdaebd26e8e
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-08-31 16:42:40 -0600

    Ejercicio 3 - S1

--- sesion1/ej3/Program.cs
+++ sesion1/ej3/Program.cs
@@ -2,3 +2,14 @@
 por un ciclista en el malecón de San Juan del Sur, utilizando como entradas la 
 velocidad promedio en kilómetros por hora y el tiempo recorrido, ambos proporcionados 
 por el usuario. */
+
+Console.WriteLine("Calculo de distancia recorrida en Km");
+
+Console.Write("Ingrese la velocidad promedio en Km/h: ");
+double velocidad = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ingrese el tiempo total: ");
+double tiempo = Convert.ToDouble(Console.ReadLine());
+
+double distancia = velocidad * tiempo;
+Console.WriteLine($"La distancia total recorrida es de {distancia}Km");
