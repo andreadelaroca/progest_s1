b09a3bbfbf81c48a33189a73c26da576803a9a29
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-08-31 20:36:44 -0600

    Ejercicio 1 - S2

--- sesion2/ej1/Program.cs
+++ sesion2/ej1/Program.cs
@@ -1,2 +1,20 @@
 ﻿// Determinar si un número ingresado es positivo, negativo o cero usando if.
 
+Console.Write("Ingrese un numero: ");
+int num = Conver.ToInt32(Console.ReadLine());
+
+if (num == 0)
+{
+    Console.WriteLine($"{num} es 0");
+}
+else
+{
+    if (num > 0)
+    {
+        Console.WriteLine($"{num} es positivo");
+    }
+    else
+    {
+        Console.WriteLine($"{num} es negativo");
+    }
+}
