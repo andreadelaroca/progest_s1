473da929cf0970dd9013debedf2035011e8fd032
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-08-31 20:26:38 -0600

    Ejercicio 1 - S2

--- sesion2/ej5/Program.cs
+++ sesion2/ej5/Program.cs
@@ -1,1 +1,13 @@
 ﻿// Crear un programa que determine si un año es bisiesto usando if y operadores lógicos.
+
+Console.Write("Ingrese un año: ");
+int bisiesto = Convert.ToInt32(Console.ReadLine());
+
+if (bisiesto % 4 == 0 && bisiesto % 100 != 0 || bisiesto % 400 == 0)
+{
+    Console.WriteLine($"{bisiesto} es un año bisiesto");
+}
+else
+{
+    Console.WriteLine($"{bisiesto} no es un año bisiesto");
+}
