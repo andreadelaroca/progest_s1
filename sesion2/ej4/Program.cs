07e7d7febf99152f4bd9f56e362ae3023ec9cc16
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-08-31 21:20:14 -0600

    Ejercicio 4 - S2

--- sesion2/ej4/Program.cs
+++ sesion2/ej4/Program.cs
@@ -1,1 +1,37 @@
 ﻿// Repetir el ej3 usando switch con rangos aproximados.
+
+Console.Write("Ingrese su nota: ");
+int nota = Convert.ToDouble(Console.ReadLine());
+char n;
+
+if (70 <= nota <= 100)
+{
+    n = 'A';
+}
+else if (50 <= nota <= 69)
+{
+    n = 'B';
+}
+else if (0 <= nota <= 50)
+{
+    n = 'C';
+}
+else
+{
+    Console.WriteLine("Nota invalida");
+}
+
+switch (n)
+{
+    case 'A':
+        Console.WriteLine("Aprobo");
+        break;
+    case 'B':
+        Console.WriteLine("Debe asistir a recuperacion");
+        break;
+    case 'C':
+        Console.WriteLine("Reprobo");
+        break;
+    default:
+        break;
+}
