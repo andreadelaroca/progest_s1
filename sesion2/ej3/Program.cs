0b82b881c9f7952360b4459b3105abd1cf64f529
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-08-31 20:53:21 -0600

    Ejercicio 3 - S2

--- sesion2/ej3/Program.cs
+++ sesion2/ej3/Program.cs
@@ -1,2 +1,22 @@
 ﻿/* Evaluar la calificación de un estudiante y mostrar si aprobó (≥70), 
 recuperación (50–69) o reprobó (<50) con if. */
+
+Console.Write("Ingrese su nota: ");
+double nota = Convert.ToDouble(Console.ReadLine());
+
+if (100 >= nota >= 70)
+{
+    Console.WriteLine("Aprobo");
+}
+else if (50 <= nota <= 69)
+{
+    Console.WriteLine("Debe asistir a recuperacion");
+}
+else if (0 <= nota <= 50)
+{
+    Console.WriteLine("Reprobo");
+}
+else
+{
+    Console.WriteLine("Nota invalida");
+}
